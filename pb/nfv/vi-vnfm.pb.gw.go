8a05fc4ebfdd21aed17c3c19f6c25c38367d0d2e
DiMalovanyy <dmitrymalovanyy@gmail.com>
2025-01-18 22:31:21 +0200

    Delete compute flavour specify Id in http path

--- pb/nfv/vi-vnfm.pb.gw.go
+++ pb/nfv/vi-vnfm.pb.gw.go
@@ -221,13 +221,22 @@ func local_request_ViVnfm_QueryComputeFlavour_0(ctx context.Context, marshaler r
 	return msg, metadata, err
 }
 
-var filter_ViVnfm_DeleteComputeFlavour_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+var filter_ViVnfm_DeleteComputeFlavour_0 = &utilities.DoubleArray{Encoding: map[string]int{"computeFlavourId": 0, "value": 1}, Base: []int{1, 1, 1, 0}, Check: []int{0, 1, 2, 3}}
 
 func request_ViVnfm_DeleteComputeFlavour_0(ctx context.Context, marshaler runtime.Marshaler, client ViVnfmClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq DeleteComputeFlavourRequest
 		metadata runtime.ServerMetadata
+		err      error
 	)
+	val, ok := pathParams["computeFlavourId.value"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "computeFlavourId.value")
+	}
+	err = runtime.PopulateFieldFromPath(&protoReq, "computeFlavourId.value", val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "computeFlavourId.value", err)
+	}
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
@@ -242,7 +251,16 @@ func local_request_ViVnfm_DeleteComputeFlavour_0(ctx context.Context, marshaler 
 	var (
 		protoReq DeleteComputeFlavourRequest
 		metadata runtime.ServerMetadata
+		err      error
 	)
+	val, ok := pathParams["computeFlavourId.value"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "computeFlavourId.value")
+	}
+	err = runtime.PopulateFieldFromPath(&protoReq, "computeFlavourId.value", val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "computeFlavourId.value", err)
+	}
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
@@ -491,7 +509,7 @@ func RegisterViVnfmHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/kubenvf.kubevim.api.pb.ViVnfm/DeleteComputeFlavour", runtime.WithHTTPPathPattern("/vivnfm/v5/flavours"))
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/kubenvf.kubevim.api.pb.ViVnfm/DeleteComputeFlavour", runtime.WithHTTPPathPattern("/vivnfm/v5/flavours/{computeFlavourId.value}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -711,7 +729,7 @@ func RegisterViVnfmHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/kubenvf.kubevim.api.pb.ViVnfm/DeleteComputeFlavour", runtime.WithHTTPPathPattern("/vivnfm/v5/flavours"))
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/kubenvf.kubevim.api.pb.ViVnfm/DeleteComputeFlavour", runtime.WithHTTPPathPattern("/vivnfm/v5/flavours/{computeFlavourId.value}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -785,7 +803,7 @@ var (
 	pattern_ViVnfm_AllocateVirtualisedComputeResource_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vivnfm", "v5", "compute"}, ""))
 	pattern_ViVnfm_CreateComputeFlavour_0                = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vivnfm", "v5", "flavours"}, ""))
 	pattern_ViVnfm_QueryComputeFlavour_0                 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vivnfm", "v5", "flavours"}, ""))
-	pattern_ViVnfm_DeleteComputeFlavour_0                = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vivnfm", "v5", "flavours"}, ""))
+	pattern_ViVnfm_DeleteComputeFlavour_0                = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"vivnfm", "v5", "flavours", "computeFlavourId.value"}, ""))
 	pattern_ViVnfm_AllocateVirtualisedNetworkResource_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vivnfm", "v5", "networks"}, ""))
 	pattern_ViVnfm_QueryVirtualisedNetworkResource_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vivnfm", "v5", "networks"}, ""))
 	pattern_ViVnfm_TerminateVirtualisedNetworkResource_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"vivnfm", "v5", "networks", "networkResourceId.value"}, ""))
